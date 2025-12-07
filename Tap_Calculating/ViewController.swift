5a56778d95c8caf59e7b0ca444c7c7fffd147a06
Rayimbek Jigitekov <96482537+Jigitekov@users.noreply.github.com>
2025-12-07 16:44:55 +0500

    Initial Commit

---
+++ Tap_Calculating/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tap_Calculating
+//
+//  Created by Rayimbek Jigitekov on 07.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
